494e0f9f9a3bd91f3c26281b76bc2a4bcca47a8e
Vladislav Kostylev <kostylev.v@gmail.com>
2018-12-29 10:31:53 +0300

    Stage 2

--- client/src/main/java/org/hyperskill/database/clientside/Client.java
+++ client/src/main/java/org/hyperskill/database/clientside/Client.java
@@ -1,7 +1,34 @@
 package org.hyperskill.database.clientside;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Client {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  private static final String SERVER_ADDRESS = "127.0.0.1";
+  private static final int SERVER_PORT = 34522;
+
+  public static void main(String[] args) {
+      try (
+          Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+          DataInputStream input = new DataInputStream(socket.getInputStream());
+          DataOutputStream output  = new DataOutputStream(socket.getOutputStream())
+      ) {
+          System.out.println("Client started!");
+          while (true) {
+            System.out.print("Give me a record # ");
+            Scanner scanner = new Scanner(System.in);
+            String msg = scanner.nextLine();
+
+                output.writeUTF(msg); // sending message to the server
+                System.out.println("Sent: " + msg);
+                String receivedMsg = input.readUTF(); // response message
+                System.out.println("Recieved: " + receivedMsg);
+
+
+          }
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
 }
--- server/src/main/java/org/hyperskill/database/serverside/Server.java
+++ server/src/main/java/org/hyperskill/database/serverside/Server.java
@@ -1,7 +1,49 @@
 package org.hyperskill.database.serverside;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+//java -cp target/...jar com.kkk.app
+
 public class Server {
+    private static final int PORT = 34522;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            while (true) {
+                Session session = new Session(server.accept());
+                session.run(); // it does not block this thread
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Session {
+    private final Socket socket;
+
+    public Session(Socket socketForClient) {
+        this.socket = socketForClient;
+    }
+
+    public void run() {
+        try (
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+        ) {
+          System.out.println("Server started!");
+            while(true) {
+                String msg = input.readUTF();
+
+                  System.out.println("Recieved: " + msg);
+                  output.writeUTF(msg);
+                  System.out.println("Sent: " + msg);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
